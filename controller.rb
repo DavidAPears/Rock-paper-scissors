989f9b8a1534b35fd3e52ac8302f0f11cd6db14a
James Henderson <hendersonjamesalexander@gmail.com>
2018-06-12 16:31:17 +0100

    MVP, basic extensions, homepage, CSS

--- controller.rb
+++ controller.rb
@@ -4,6 +4,10 @@ require('sinatra/contrib/all')
 require_relative('./models/game.rb')
 also_reload( './models/*' )
 
+get '/'do
+  erb(:home)
+end
+
 get '/rps_game/:player1/:player2' do
   game = Game.new( params[:player1], params[:player2] )
   @outcome = game.rps_game()
