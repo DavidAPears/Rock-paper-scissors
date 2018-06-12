9601669470abce46679314978728a82ed0c3dfa6
James Henderson <hendersonjamesalexander@gmail.com>
2018-06-12 16:10:39 +0100

    Fully built model updated

--- models/game.rb
+++ models/game.rb
@@ -12,7 +12,7 @@ class Game
       return "Paper wins!"
     elsif (@player1 == "scissors" && @player2 == "paper") || (@player2 == "paper" && @player1 == "scissors")
       return "Scissors wins!"
-    elsif (@player1 == @player2 && @player2 == @player1) || (@player2 == @player1 && @player1 == @player2)
+    elsif @player1 == @player2
       return "Draw!"
     end
   end
